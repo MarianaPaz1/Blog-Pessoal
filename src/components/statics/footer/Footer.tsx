aafce3dcedd08a0d837b408f0d8c337fb5d05244
Mariana <mnp1807@hotmail.com>
2023-04-11 17:33:36 -0300

    alterando footer

--- src/components/statics/footer/Footer.tsx
+++ src/components/statics/footer/Footer.tsx
@@ -4,9 +4,12 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import Mariana from './src/assets/Mariana.jpeg'
-import footer from './Footer.css'
 
-export const Footer: FC = (): ReactElement => {
+
+function Footer(){
+    
+}
+export const footer: FC = (): ReactElement => {
     return (
         <Box
             sx={{
